c7d0b9138b61f3f7e10838b3374d1c6c89b61b69
Noodlemanifold <143763734+noodlemanifold@users.noreply.github.com>
2024-12-07 22:43:57 -0800

    Primitive Meshes

--- Assets/TestScript.cs
+++ Assets/TestScript.cs
@@ -1,8 +1,12 @@
 
+using System;
 using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
 
 public class TestScript : MonoBehaviour {
-    
-    
+    private void Start() {
+        
+    }
 }
 
